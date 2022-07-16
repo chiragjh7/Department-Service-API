c82999929f9c49772389f5703ec7420c679a98b3
Chirag Jhariya <chiragjhariya7@gmail.com>
2022-07-16 15:24:56 +0530

    changed H2 to MySQL

--- src/main/java/com/logncodes/Springboot/demo/controller/DepartmentController.java
+++ src/main/java/com/logncodes/Springboot/demo/controller/DepartmentController.java
@@ -1,12 +1,16 @@
 package com.logncodes.Springboot.demo.controller;
 
 import com.logncodes.Springboot.demo.entity.Department;
+import com.logncodes.Springboot.demo.error.DepartmentNotFoundException;
 import com.logncodes.Springboot.demo.service.DepartmentService;
 import com.logncodes.Springboot.demo.service.DepartmentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.GeneratedValue;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,18 +19,23 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside save department of department controller");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDepartmentList of Department Controller");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
         return departmentService.fetchDepartmentById(departmentId);
     }
 
--- src/main/java/com/logncodes/Springboot/demo/entity/Department.java
+++ src/main/java/com/logncodes/Springboot/demo/entity/Department.java
@@ -1,70 +1,46 @@
 package com.logncodes.Springboot.demo.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+
+    // Hibernate Different Validation
+//    @Length(max = 5, min =  1)
+//    @Size(max = 10, min = 0)
+//    @Email
+//    @Positive
+//    @Negative
+//    @PositiveOrZero
+//    @NegativeOrZero
+//    @Future
+//    @FutureOrPresent
+//    @Past
+//    @PastOrPresent
+    @NotBlank(message = "Please Add department Name")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
 
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
 
-    public Department() {
-    }
 
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
 }
